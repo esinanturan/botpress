7e74fcec0d51941be5e2c4d111ca1914fa8b9bd9
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-12-17 16:55:53 -0500

    refactor: simplify states

--- integrations/trello/definitions/states.ts
+++ integrations/trello/definitions/states.ts
@@ -1,13 +1,17 @@
-import { IntegrationDefinitionProps } from '@botpress/sdk'
-import { webhookStateSchema } from './schemas'
-
-export const States = {
-  webhookState: 'webhookState',
-} as const
+import { type IntegrationDefinitionProps, z } from '@botpress/sdk'
+import { trelloIdSchema } from './schemas/entities'
 
 export const states = {
-  [States.webhookState]: {
+  webhookState: {
     type: 'integration',
-    schema: webhookStateSchema,
+    schema: z
+      .object({
+        trelloWebhookId: trelloIdSchema
+          .or(z.null())
+          .title('Trello Webhook ID')
+          .describe('Unique id of the webhook that is created upon integration registration')
+          .default(null),
+      })
+      .describe('State that stores the webhook id for the Trello integration'),
   },
 } as const satisfies NonNullable<IntegrationDefinitionProps['states']>
